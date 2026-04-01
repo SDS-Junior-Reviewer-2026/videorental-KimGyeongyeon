c18fd30a3077dc54d09dc46290c24066ba096309
KimGyeongyeon <kgy556@naver.com>
2026-04-01 15:55:51 +0900

    3-4. regular movie 계산로직 테스트

--- src/test/java/com/videorental/CustomerStatementTest.java
+++ src/test/java/com/videorental/CustomerStatementTest.java
@@ -8,6 +8,11 @@ public class CustomerStatementTest {
     private Customer createCustomer(String name) {
         return new Customer(name);
     }
+
+    private Rental regularRent(String title, int day) {
+        return new Rental(new Movie(title, Movie.REGULAR), day);
+    }
+
     @Test
     void given_NoMovie_then_printZeroStatement() {
         Customer customer = createCustomer("ANY_NAME");
@@ -19,6 +24,40 @@ public class CustomerStatementTest {
         assertThat(stmtResult).isEqualTo("Rental Record for ANY_NAME\n"
                 +"Amount owed is 0.0\n"
         +"You earned 0 frequent renter pointers"
+        );
+    }
+
+    @Test
+    void given_RegularMovieFor2day_then_printStatement() {
+        Customer customer = createCustomer("ANY_NAME");
+
+        // when
+        customer.addRental(regularRent("Regular Movie", 2));
+
+        // then
+        String stmtResult = customer.statement();
+        assertThat(stmtResult).isEqualTo(
+                "Rental Record for ANY_NAME\n" +
+                        "\t2.0(Regular Movie)\n" +
+                        "Amount owed is 2.0\n" +
+                        "You earned 1 frequent renter pointers"
+        );
+    }
+
+    @Test
+    void given_RegularMovieFor3day_then_printStatement() {
+        Customer customer = createCustomer("ANY_NAME");
+
+        // when
+        customer.addRental(regularRent("Regular Movie", 3));
+
+        // then
+        String stmtResult = customer.statement();
+        assertThat(stmtResult).isEqualTo(
+                "Rental Record for ANY_NAME\n" +
+                        "\t3.5(Regular Movie)\n" +
+                        "Amount owed is 3.5\n" +
+                        "You earned 1 frequent renter pointers"
         );
     }
 }
