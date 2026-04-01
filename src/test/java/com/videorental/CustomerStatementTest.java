447c00f11652e996e950b063e6f4465e194c5986
KimGyeongyeon <kgy556@naver.com>
2026-04-01 15:48:39 +0900

    2. Movie rental하지 않은 경우 테스트 추가

--- src/test/java/com/videorental/CustomerStatementTest.java
+++ src/test/java/com/videorental/CustomerStatementTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CustomerStatementTest {
+    private Customer createCustomer(String name) {
+        return new Customer(name);
+    }
     @Test
     void given_NoMovie_then_printZeroStatement() {
-        Customer customer = new Customer("ANY_NAME");
+        Customer customer = createCustomer("ANY_NAME");
 
         // when
         String stmtResult = customer.statement();
@@ -16,7 +19,6 @@ public class CustomerStatementTest {
         assertThat(stmtResult).isEqualTo("Rental Record for ANY_NAME\n"
                 +"Amount owed is 0.0\n"
         +"You earned 0 frequent renter pointers"
-
         );
     }
 }
